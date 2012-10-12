764365725bfc851e55a6d6b3f8b2c2c90edd0810
RAYA DESAWADE <rayalynn@gmail.com>
2012-10-12 13:20:05 +0700

    Created app.

---
+++ README.md
@@ -0,0 +1,1 @@
+Web interface for my website scraper to kindle.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KindleSite::Application.config.secret_token = '40e234988123b51e89ab8dfa94a6371b38a91f69487931b5074f0b39480b0c940055bf81adfe3defcfc6178268e6e8d3c91dda08c3da9199955192f87963f4f7'
