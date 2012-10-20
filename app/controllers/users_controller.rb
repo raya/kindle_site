1a10581fde4e292eaa957bc754d15c1508039357
RAYA DESAWADE <rayalynn@gmail.com>
2012-10-20 15:52:58 +0700

    Removed Trog gem from gemfile. Cannot figure out how to use it in Rails. Going to execute script manually for now.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,6 @@
 class UsersController < ApplicationController
-  require 'Trog_Scraper'
+
 
   def new
-    #test = TrogScraper.new("google.com")
-    test = TrogScraper::HtmlGenerator.random_filename
   end
 end
