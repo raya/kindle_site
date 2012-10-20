a1a21c10b98bd0ee613be977ca6149bbf6e59ce4
RAYA DESAWADE <rayalynn@gmail.com>
2012-10-20 20:31:23 +0700

    Added validations to site arguments model

--- app/models/site_arg.rb
+++ app/models/site_arg.rb
@@ -16,4 +16,12 @@
 
 class SiteArg < ActiveRecord::Base
   attr_accessible :filename, :max_entries, :next_post, :post_matcher, :starting_page, :starting_page_inc, :url
+
+  validates :url, presence: true
+  validates :max_entries, presence: true, numericality: { only_integer: true }, length: { minimum: 1 }
+  validates :next_post, presence: true
+  validates :post_matcher, presence: true
+  validates :starting_page, presence: true
+  validates :starting_page_inc, presence: true, numericality: { only_integer: true }
+
 end
